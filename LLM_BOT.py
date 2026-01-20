1cec3c00a240c550238601977bc45f581fa20f3b
suhani090505 <sumansuhani852@gmail.com>
2026-01-20 02:08:20 -0800

    Update LLM_BOT.py

--- LLM_BOT.py
+++ LLM_BOT.py
@@ -3,6 +3,8 @@ import streamlit as st
 from groq import Groq
 
 st.set_page_config("PragyanAI Content Generator", layout="wide")
+st.image("photo.jpg", use_container_width=True)
+
 st.title("📢 Suhani's – Content Generator")
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
